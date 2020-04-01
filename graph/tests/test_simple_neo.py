a58a8c3053ad88ee3214040c8fbfd6241ac382e9
Korovin Viktor <sarvash.inc@gmail.com>
2020-04-01 16:24:21 +0700

    Hm

---
+++ graph/tests/test_simple_neo.py
@@ -0,0 +1,29 @@
+import asyncio
+
+import pytest
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_simple():
+    from neo4j import GraphDatabase
+
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "test"))
+
+    def add_friend(tx, name, friend_name):
+        tx.run("MERGE (a:Person {name: $name}) "
+               "MERGE (a)-[:KNOWS]->(friend:Person {name: $friend_name})",
+               name=name, friend_name=friend_name)
+
+    def print_friends(tx, name):
+        for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
+                             "RETURN friend.name ORDER BY friend.name", name=name):
+            assert record["friend.name"]
+
+    with driver.session() as session:
+        session.write_transaction(add_friend, "Arthur", "Guinevere")
+        session.write_transaction(add_friend, "Arthur", "Lancelot")
+        session.write_transaction(add_friend, "Arthur", "Merlin")
+        session.read_transaction(print_friends, "Arthur")
+
+    driver.close()
