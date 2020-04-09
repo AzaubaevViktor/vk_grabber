50534311672655253559a48d14c07ffb088569fa
Korovin Viktor <sarvash.inc@gmail.com>
2020-04-09 23:20:46 +0700

    Add tests for find

---
+++ database/tests/test_find_kwargs.py
@@ -0,0 +1,54 @@
+import pytest
+
+from database import Model, ModelAttribute
+
+
+pytestmark = pytest.mark.asyncio
+
+
+class TParams(Model):
+    p = ModelAttribute()
+
+
+async def test_find_kwargs(db):
+    await db.insert_many(*(
+        TParams(p=i) for i in range(10)
+    ))
+
+    found_items = []
+
+    async for item in db.find(TParams(), limit=5, processed=None):
+        assert isinstance(item, TParams)
+        assert not item.updates()
+        found_items.append(item)
+
+    assert len(found_items) == 5
+
+    for item in found_items:
+        item.p *= 2
+
+    db.insert_many(*found_items, processed=True)
+
+    not_processed_yet = []
+
+    async for item in db.find(TParams(), processed=None):
+        assert isinstance(item, TParams)
+        assert not item.updates()
+        not_processed_yet.append(item)
+
+    assert len(not_processed_yet) == 5
+
+    already_processed = []
+
+    async for item in db.find(TParams(), processed=True):
+        assert isinstance(item, TParams)
+        assert not item.updates()
+        already_processed.append(item)
+
+    assert len(already_processed) == 5
+
+    assert not set(
+        item._id for item in not_processed_yet
+    ).intersection(set(
+        item._id for item in already_processed
+    ))
