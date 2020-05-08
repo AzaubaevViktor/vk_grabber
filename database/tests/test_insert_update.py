c0997c73650d2c314a8b77b81d2e9b90cd2d5e1a
Korovin Viktor <sarvash.inc@gmail.com>
2020-05-08 20:41:14 +0700

    Write tests for insert/update

---
+++ database/tests/test_insert_update.py
@@ -0,0 +1,49 @@
+import pytest
+
+from database import Model, ModelAttribute, DBWrapper
+
+
+pytestmark = pytest.mark.asyncio
+
+
+class OneID(Model):
+    a = ModelAttribute(uid=True)
+    b = ModelAttribute()
+    c = ModelAttribute()
+
+
+async def test_insert(db: DBWrapper):
+    obj = OneID(a=1, b=2, c=3)
+    await db.insert_many(obj)
+
+    obj = OneID(a=2, b=2, c=3)
+    await db.insert_many(obj)
+
+    assert await db.count(OneID) == 2
+
+
+@pytest.mark.parametrize('variant', (0, 1))
+async def test_insert_update(db: DBWrapper, variant):
+    obj1 = OneID(a=1, b=2, c=3)
+    await db.insert_many(obj1)
+
+    obj2 = await db.find_one(OneID(a=1))
+
+    assert obj1 == obj2
+
+    obj1.b = 3
+    obj2.c = 4
+
+    if variant == 0:
+        await db.insert_many(obj1, obj2)
+    else:
+        await db.insert_many(obj1)
+        await db.insert_many(obj2)
+
+    assert db.count(OneID) == 1
+
+    result = await db.find_one(OneID(a=1))
+
+    assert result.a == 1
+    assert result.b == 3
+    assert result.c == 4
