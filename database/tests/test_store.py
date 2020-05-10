e4c08ea0cf781195d13039b6812da819a0a12651
Korovin Viktor <sarvash.inc@gmail.com>
2020-05-10 15:12:56 +0700

    Add tests for TwoID

--- database/tests/test_store.py
+++ database/tests/test_store.py
@@ -15,6 +15,7 @@ class TwoID(Model):
     a = ModelAttribute(uid=True)
     b = ModelAttribute(uid=True)
     c = ModelAttribute()
+    d = ModelAttribute(default=None)
 
 
 class ThreeId(Model):
@@ -36,6 +37,19 @@ async def test_store_new(db):
     assert m == result_kwarg
 
 
+async def test_store_new_2(db):
+    m = TwoID(a=1, b=2, c=3)
+    await db.store(m)
+
+    result_dict = await db.find_one(TwoID, {'a': 1, 'b': 2})
+    assert isinstance(result_dict, TwoID)
+    assert m == result_dict
+
+    result_kwarg = await db.find_one(TwoID, a=1, b=2)
+    assert isinstance(result_kwarg, TwoID)
+    assert m == result_kwarg
+
+
 async def test_store_exist_update(db):
     m = OneID(a=1, b=2)
     n = OneID(a=1, c=3)
@@ -47,6 +61,22 @@ async def test_store_exist_update(db):
     assert result.a == 1
     assert result.b == 2
     assert result.c == 3
+
+
+async def test_store_exist_update_2(db):
+    m = TwoID(a=1, b=2, c=3)
+    n = TwoID(a=1, b=2, d=4)
+
+    await db.store(m)
+    await db.store(n)
+
+    assert await db.count(TwoID) == 1
+
+    result = await db.find_one(TwoID, a=1)
+    assert result.a == 1
+    assert result.b == 2
+    assert result.c == 3
+    assert result.d == 4
 
 
 async def test_store_exist_rewrite(db):
@@ -66,3 +96,28 @@ async def test_store_exist_rewrite(db):
     assert result.a == 1
     assert result.b is None
     assert result.c == 3
+
+
+async def test_store_exist_rewrite_2(db):
+    m = TwoID(a=1, b=2, c=3)
+    n = TwoID(a=1, b=2, d=4)
+
+    await db.store(m)
+
+    assert await db.count(TwoID) == 1
+
+    result = await db.find_one(TwoID, a=1)
+    assert result.a == 1
+    assert result.b == 2
+    assert result.c == 3
+    assert result.d is None
+
+    await db.store(n)
+
+    assert await db.count(TwoID) == 1
+
+    result = await db.find_one(TwoID, a=1)
+    assert result.a == 1
+    assert result.b == 2
+    assert result.c is None
+    assert result.d == 4
