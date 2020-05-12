cd31c422df0829d12d076df673b78f877c6c8591
Korovin Viktor <sarvash.inc@gmail.com>
2020-05-13 00:22:03 +0700

    Remove deprecated methods from wrapper

--- database/db_wrapper.py
+++ database/db_wrapper.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-from typing import Union, Type, Dict, List, TypeVar, AsyncIterable, Optional
+from typing import Union, Type, Dict, TypeVar, AsyncIterable, Optional
 
 import motor.motor_asyncio
 
@@ -152,8 +151,6 @@ class DBWrapper:
         async for raw_item in self.choose_raw(ModelClass=ModelClass, query_=query_, updates_=updates_, limit_=limit_, sort_=sort_):
             yield self._transform(ModelClass, raw_item)
 
-    # DEPRECATED
-
     def _get_collection(self, obj: ModelCollectionT):
         if isinstance(obj, Model):
             klass = obj.__class__
@@ -167,107 +164,11 @@ class DBWrapper:
 
         return self._collections[klass]
 
-    async def insert_many(self, *objs: Model, force=False, **kwargs):
-        for item in objs:
-            collection = self._get_collection(item)
-            collection.update_one({})
-
-        # Divide by classes
-        classes: Dict[Type[Model], List[Model]] = defaultdict(list)
-
-        for obj in objs:
-            try:
-                obj.verificate()
-            except ValueError:
-                if not force:
-                    raise
-
-            classes[obj.__class__].append(obj)
-
-        # TODO: Parallel this please
-
-        for klass, items in classes.items():
-            # TODO: Insert with update if exist
-            collection = self._get_collection(klass)
-
-            items_insert = []
-
-            for item in items:
-                if item._id is not None:
-                    if await collection.count_documents({'_id': item._id}, {'limit': 1}):
-                        pass
-
-            results = await collection.insert_many([
-                {**item.serialize(), **kwargs} for item in items
-            ])
-
-            for obj, _id in zip(items, results.inserted_ids):
-                obj._id = _id
-
     def _transform(self, obj: Union[Type[Model], Model], item_raw):
         ModelType = type(obj) if not isinstance(obj, type) else obj
         item = ModelType.soft_create(**item_raw)
         item.drop_updates()
         return item
-
-    async def _find(self, obj: ModelT, limit=None, **kwargs) -> AsyncIterable[ModelT]:
-        collection = self._get_collection(obj)
-        query = obj.query()
-        query.update(kwargs)
-        cursor = collection.find(query)
-        if limit is not None:
-            cursor = cursor.limit(limit)
-
-        async for item_raw in cursor:
-            yield self._transform(obj, item_raw)
-
-    async def _find_one(self, obj: Model):
-        collection = self._get_collection(obj)
-        item_raw = await collection.find_one(obj.query())
-        return self._transform(obj, item_raw)
-
-    async def update_model(self, obj: Model, **params):
-        collection = self._get_collection(obj)
-
-        assert obj._id is not None
-        updates = obj.updates()
-        updates.update(params)
-        assert updates
-
-        await collection.update_one(
-            {'_id': obj._id},
-            {'$set': updates}
-        )
-
-        obj.drop_updates()
-
-    async def update(self, query: dict, upd: dict, limit=None) -> AsyncIterable[ModelT]:
-        Class = query['@model']
-        del query['@model']
-        collection = self._get_collection(Class)
-
-        # Find ids by query
-
-        # TODO: Get only id's
-        cursor = collection.find(query, projection=['_id'])
-        if limit is not None:
-            cursor = cursor.limit(limit)
-
-        ids = []
-        async for item in cursor:
-            ids.append(item['_id'])
-
-        # Update by upd
-        new_query = {'_id': {'$in': ids}}
-        result = await collection.update_many(
-            new_query,
-            {'$set': upd}
-        )
-
-        # Get updated items by id
-
-        async for item_raw in collection.find(new_query):
-            yield self._transform(Class, item_raw)
 
     async def delete_all(self, i_understand_delete_all=False):
         assert i_understand_delete_all
