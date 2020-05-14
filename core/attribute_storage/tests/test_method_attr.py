2cbf058fb9467e6abdac8df937dc295eefe24c98
Korovin Viktor <sarvash.inc@gmail.com>
2020-05-14 16:53:54 +0700

    Add attribute property tests

---
+++ core/attribute_storage/tests/test_method_attr.py
@@ -0,0 +1,32 @@
+import pytest
+
+from core import AttributeStorage, Attribute
+
+
+class MethodOne(AttributeStorage):
+    def _calc(self):
+        return self.a * 2
+
+    a = Attribute()
+    a2 = Attribute(method=_calc)
+
+
+class MethodTwo(AttributeStorage):
+    a = Attribute()
+
+    @Attribute.property
+    def a2(self):
+        return self.a * 2
+
+
+@pytest.mark.parametrize('Class', (
+    MethodOne, MethodTwo
+))
+def test_property(Class):
+    obj = Class(a=10)
+
+    assert obj.a == 10
+    assert obj.a2 == 10
+
+    obj.a = 20
+    assert obj.a2 == 40
