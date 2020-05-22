cbc4c84182a09d2c40273a575c00391b2eab1d0f
Korovin Viktor <sarvash.inc@gmail.com>
2020-05-22 20:52:08 +0700

    Add State

--- dyploma/models.py
+++ dyploma/models.py
@@ -1,6 +1,18 @@
+from enum import Enum
 from time import time
 
 from database import Model, ModelAttribute
+
+
+class State(Enum):
+    NEW = None
+    IN_PROGRESS = "IN_PROGRESS"
+    FINISHED = "FINISHED"
+
+    ERROR = "ERROR"
+    SKIP = "SKIP"
+
+    DELETE = "DELETE"
 
 
 class Word(Model):
