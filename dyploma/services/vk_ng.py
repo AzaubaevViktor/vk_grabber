da8154a2511dc9b01bcd9e6c201433b79b65189d
Korovin Viktor <sarvash.inc@gmail.com>
2020-05-22 21:25:09 +0700

    Write new Works

---
+++ dyploma/services/vk_ng.py
@@ -0,0 +1,96 @@
+from app.base import BaseWorkApp, AppContext
+from dyploma.models import State
+from dyploma.services._base import _ChooseModelByField
+from vk_utils import VKGroup, VKPerson, VKPost
+
+
+# GROUP
+
+class LoadGroups(BaseWorkApp):
+    INPUT_RETRIES = 0
+
+    async def input(self):
+        for item in self.ctx.started_groups:
+            yield item
+
+    async def process(self, item: int):
+        item = int(item)
+        yield VKGroup(id=item)
+
+    async def update(self, result):
+        await self.db.store(result, rewrite=False)
+
+
+class LoadGroupInfo(_ChooseModelByField):
+    MODEL_CLASS = VKGroup
+
+    async def process(self, group: VKGroup):
+        yield await self.vk.group_info(group_id=group.id)
+
+    async def update(self, group: VKGroup):
+        await self.db.store(group, rewrite=False)
+
+
+# PERSON
+
+
+class LoadPersonFromGroup(_ChooseModelByField):
+    MODEL_CLASS = VKGroup
+
+    async def process(self, group: VKGroup):
+        async for person_id in self.vk.group_participants_iter(group_id=group.id):
+            yield VKPerson(id=person_id)
+
+    async def update(self, person: VKPerson):
+        await self.db.store(person, rewrite=False)
+
+
+class LoadPersonInfo(_ChooseModelByField):
+    MODEL_CLASS = VKPerson
+
+    async def process(self, person: VKPerson):
+        yield await self.vk.persons_info(person.id)
+
+    async def update(self, person: VKPerson):
+        await self.db.store(person, rewrite=False)
+
+
+# POST
+
+
+class _PostLoader(_ChooseModelByField):
+    def __init__(self, ctx: AppContext):
+        super().__init__(ctx)
+        self.posts_count = self.ctx.posts_count
+
+    async def update(self, post: VKPost):
+        if not post.text:
+            return
+
+        await self.db.store(post, rewrite=False)
+
+
+class LoadPostFromGroup(_PostLoader):
+    MODEL_CLASS = VKGroup
+
+    async def process(self, group: VKGroup):
+        async for post in self.vk.group_posts_iter(group_id=group.id,
+                                                   count=self.posts_count):
+            yield post
+
+
+class LoadPostFromPerson(_PostLoader):
+    MODEL_CLASS = VKPerson
+
+    ADDITIONAL_FILTER = {
+        # TODO: After metaclass, Use .FIELD_NAME
+        LoadPersonInfo.__name__: State.FINISHED,
+        VKPerson.deactivated.name: False,
+        VKPerson.hidden.name: False,
+        VKPerson.is_closed.name: False,
+    }
+
+    async def process(self, person: VKPerson):
+        async for post in self.vk.person_posts_iter(person_id=person.id,
+                                                    count=self.posts_count):
+            yield post
--- dyploma/services/word_.py
+++ dyploma/services/word_.py
@@ -1,45 +1,21 @@
-from app.base import BaseWorkApp
-from core import BaseWork
-from dyploma.models import Word
 from vk_utils import VKPost, VKComment
 from word_worker import tokenize
 
-
-class _LoadModelField(BaseWorkApp):
-    MODEL_CLASS = None
-    FIELD_NAME = None
-    INPUT_QUERY_LIMIT = 5
-
-    async def warm_up(self):
-        # Clean field=False
-        assert self.MODEL_CLASS
-        assert self.FIELD_NAME
-
-        count = 0
-        async for _ in self.db.choose(self.MODEL_CLASS, {self.FIELD_NAME: False},
-                                      {self.FIELD_NAME: None}):
-            count += 1
-
-        self.log.info("Cleanup old tasks", count=count)
-
-    async def input(self):
-        async for item in self.db.choose(self.MODEL_CLASS, {self.FIELD_NAME: None},
-                                         {self.FIELD_NAME: False},
-                                         limit_=self.INPUT_QUERY_LIMIT):
-            yield item, self.db.store(item, **{self.FIELD_NAME: True})
+from ..models import Word
+from ._base import _ChooseModelByField
 
 
-class BaseWordKnife(_LoadModelField):
+class BaseWordKnife(_ChooseModelByField):
     INPUT_RETRIES = 5
     MODEL_CLASS = None
-    FIELD_NAME = 'word_processed'
 
     async def process(self, post: VKPost):
-        for word in tokenize(post.text):
+        for pos, word in enumerate(tokenize(post.text)):
             yield Word(word=word,
                        post_id=post.id,
                        from_id=post.from_id,
-                       date=post.date, )
+                       date=post.date,
+                       position=pos)
 
     async def update(self, word: Word):
         await self.db.store(
@@ -53,8 +29,3 @@ class WordKnifePost(BaseWordKnife):
 
 class WordKnifeComment(BaseWordKnife):
     MODEL_CLASS = VKComment
-
-
-class FoundMaxWords(BaseWork):
-    async def input(self):
-        pass
