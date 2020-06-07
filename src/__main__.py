1b4d93a5b1e3bdf3bf7678674a6123c47931949c
Korovin Viktor <sarvash.inc@gmail.com>
2020-06-07 14:56:05 +0700

    Error check

--- src/__main__.py
+++ src/__main__.py
@@ -6,6 +6,11 @@ from core import LoadConfig
 
 
 async def main():
+    if len(argv) != 2:
+        app_name = argv[0] if argv else "app"
+        print(f"Please use {app_name} <path/to/config_file.yaml>")
+        print("Instead:")
+        print(argv)
     app = DyplomaApplication(LoadConfig(argv[1]))
     await app.warm_up()
     await app()
